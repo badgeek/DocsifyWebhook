3c8b83b3e9d5a1352ce9a6aff2792f3cfab35cc5
Budi Prakosa <iyok@deadmediafm.org>
2017-07-20 08:26:00 +0700

    fix path

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define("TOKEN", "secret-token");                                       // The secret token to add as a GitHub or GitLab secret, or otherwise as https://www.example.com/?token=secret-token
 define("REMOTE_REPOSITORY", "git@github.com:8BitMixtape/NeoWiki.git"); // The SSH URL to your repository
-define("DIR", "/home4/lpatch/deadmediafm/manticore.deadmediafm.org/DocsifyWebhook/8Bit-Mixtape-NEO.wiki");                          // The path to your repostiroy; this must begin with a forward slash (/)
+define("DIR", "/home4/lpatch/deadmediafm/manticore.deadmediafm.org/DocsifyWebhook/8Bit-Mixtape-NEO.wiki/");                          // The path to your repostiroy; this must begin with a forward slash (/)
 define("BRANCH", "refs/heads/master");                                 // The branch route
 define("LOGFILE", "deploy.log");                                       // The name of the file you want to log to.
 define("GIT", "/usr/bin/git");                                         // The path to the git executable
