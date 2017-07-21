99c8e6153220a090204f0b0f615199236493ac40
Budi Prakosa <iyok@deadmediafm.org>
2017-07-21 08:32:02 +0700

    gitbook tree

--- build_docs.py
+++ build_docs.py
@@ -110,7 +110,7 @@ def pull_wiki_repo():
     return True
 
 
-def generate_sidebar():
+def generate_sidebar_docsify():
     path_list = []
 
     list_dir = os.listdir(os.path.join(MKDOCS_DIR, WIKI_NAME))
@@ -135,6 +135,31 @@ def generate_sidebar():
     pages_str = "" + "\n".join(path_list) + "\n"
     return pages_str
 
+
+def generate_sidebar_gitbook():
+    path_list = []
+
+    list_dir = os.listdir(os.path.join(MKDOCS_DIR, WIKI_NAME))
+    list_dir.sort(key=natural_keys)
+
+    for file in list_dir:
+      
+
+      if file.endswith(".md"):
+        if (sub_re.match(file)):
+            tmp_file = file[:-3].replace("-", " ").replace("_",".")
+            path_list.append("  * [%s](%s)" % (tmp_file, file))
+        else:
+            path_list.append("* [%s](%s)" % (file[:-3].replace("-", " ").replace("_","."), file))
+
+    if not path_list:
+      print(("ERROR: No markdown files found in %s ! " % MKDOCS_DIR) +
+          "Check if repository has been set up correctly.")
+      return False
+    
+    pages_str = "# Summary\n\n" + "\n\n".join(path_list) + "\n"
+    return pages_str
+
 def save_sidebar():
     html_code = ""
     print("Creating the _sidebar.md file...\n")
@@ -148,7 +173,28 @@ def save_sidebar():
             return False
     try:
         sidebar_file = open(os.path.join(generated_site_dir, "dist/sidebar.md"), "w")
-        sidebar_file.write(generate_sidebar())
+        sidebar_file.write(generate_sidebar_docsify())
+        sidebar_file.close()
+        return True
+    except IOError:
+        print("ERROR: Could not create sidebar.md file in %s !\n" %
+              generated_site_dir)
+        return False
+
+def save_sidebar_gitbook():
+    html_code = ""
+    print("Creating the _sidebar.md file...\n")
+    generated_site_dir = THIS_FILE_DIR
+    if not os.path.exists(generated_site_dir):
+        try:
+            os.makedirs(generated_site_dir)
+        except IOError:
+            print("ERROR: Could not create site folder in %s !\n" %
+                  generated_site_dir)
+            return False
+    try:
+        sidebar_file = open(os.path.join(generated_site_dir, "dist/SUMMARY.md"), "w")
+        sidebar_file.write(generate_sidebar_gitbook())
         sidebar_file.close()
         return True
     except IOError:
@@ -170,4 +216,5 @@ def build_docs():
 
 
 if __name__ == "__main__":
-    build_docs()
+    print save_sidebar_gitbook()
+    # build_docs()
