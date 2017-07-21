c6967b2a8839485b4486bb003ad137fa9c5d35ac
Budi Prakosa <iyok@deadmediafm.org>
2017-07-22 00:26:46 +0700

    play

--- publish_docsify.sh
+++ publish_docsify.sh
@@ -4,6 +4,7 @@ mkdir dist
 cp -r $GIT_SOURCE_FOLDER/* dist/
 rm -fr dist/.git
 python build_docs.py docsify
+cp "template/docsify.js" dist/
 generate_package_template "index.html" "template/docsify_index.html" > dist/index.html
 rm -fr output_docsify
 cp -r dist output_docsify
