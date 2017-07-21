29c1601feff6e213517745ffb5d362e2fe579291
Budi Prakosa <iyok@deadmediafm.org>
2017-07-21 09:38:30 +0700

    dont upload

---
+++ publish_docsify.sh
@@ -0,0 +1,8 @@
+source config.sh
+cd /home4/lpatch/deadmediafm/manticore.deadmediafm.org/DocsifyWebhook/
+rm -fr dist
+mkdir dist
+python build_docs.py
+cp -r 8Bit-Mixtape-NEO.wiki dist/8Bit-Mixtape-NEO.wiki
+rm -fr dist/8Bit-Mixtape-NEO.wiki/.git
+cp index.html dist/
