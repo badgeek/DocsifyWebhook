516b2b2919c0dd40551ca8b04b98e4e8697ab441
deploy.sh <lpatch@gator3100.hostgator.com>
2017-08-24 22:14:45 -0500

    cname

--- publish_docsify.sh
+++ publish_docsify.sh
@@ -5,6 +5,7 @@ cp -r $GIT_SOURCE_FOLDER/* dist/
 rm -fr dist/.git
 python build_docs.py docsify
 cp "template/docsify.js" dist/
+cp "template/CNAME" dist/
 generate_package_template "index.html" "template/docsify_index.html" > dist/index.html
 rm -fr output_docsify
 cp -r dist output_docsify
