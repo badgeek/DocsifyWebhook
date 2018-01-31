1e88004e07947fb9eda94c8437c10ab30cdaecf2
Budi Prakosa <iyok@deadmediafm.org>
2018-01-31 08:58:02 +0700

    make config sh portable

--- config.sh
+++ config.sh
@@ -1,8 +1,11 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 export GIT_SOURCE_REPO="git@github.com:8BitMixtape/8Bit-Mixtape-NEO.wiki.git"
-export GIT_DEPLOY_BRANCH="gh-pages"
 export GIT_DEPLOY_REPO="git@github.com:8BitMixtape/NeoWiki.git"
-export WEBHOOK_FILEPATH="/home4/lpatch/deadmediafm/manticore.deadmediafm.org/DocsifyWebhook/"
-export DOCSIFY_BASEPATH="/NeoWiki/"
+export GIT_DEPLOY_DIR="output_docsify"
+export GIT_DEPLOY_BRANCH="gh-pages"
+export WEBHOOK_FILEPATH="${DIR}/"
+export DOCSIFY_BASEPATH="" #for github use example NeoWiki
 export DOCSIFY_SIDEBAR="sidebar.md"
 export DOCSIFY_NAME="8BitMixtape-NEO"
 export OUTPUT_TYPE="docsify"
